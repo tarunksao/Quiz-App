ea151fcb57c7963e6e52557f7f193aa68332da4e
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-01-26 02:27:30 +0530

    Updated game.js & game.css file

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -53,6 +55,7 @@ getNewQuestion = () => {
         return window.location.assign('/end.html');
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -81,6 +84,10 @@ choices.forEach(choice => {
             classToApply = 'correct';
         }
 
+        if (classToApply==='correct'){
+            incrementScore(CORRECT_BONUS);
+        }
+
         selectedChoice.parentElement.classList.add(classToApply);
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
@@ -88,5 +95,10 @@ choices.forEach(choice => {
         }, 1000)
     });
 });
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
 
 startGame();
